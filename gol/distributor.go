0c45a73f67ec3825a3f666873ef4d131c03cde54
layanalajeel7 <pk23085@bristol.ac.uk>
2025-11-04 21:14:21 +0000

    started stage 1 and 2

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,12 @@
 package gol
 
+import (
+	"strconv"
+
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
+// Channels used between Distributor, IO and TestGol.
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -9,24 +16,120 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
-// distributor divides the work between workers and interacts with other goroutines.
+// nextGrid runs for a section of rows (startY to endY) and returns that part of the next world.
+func nextGrid(p Params, world [][]uint8, startY, endY int) [][]uint8 {
+	height := endY - startY
+	newWorld := make([][]uint8, height)
+	for y := 0; y < height; y++ {
+		newWorld[y] = make([]uint8, p.ImageWidth)
+	}
+
+	for y := startY; y < endY; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+
+			aliveNeighbours := 0
+			for i := -1; i <= 1; i++ {
+				for j := -1; j <= 1; j++ {
+					if !(i == 0 && j == 0) {
+						ny := (y + i + p.ImageHeight) % p.ImageHeight
+						nx := (x + j + p.ImageWidth) % p.ImageWidth
+						if world[ny][nx] == 255 {
+							aliveNeighbours++
+						}
+					}
+				}
+			}
+
+			if world[y][x] == 255 {
+				if aliveNeighbours < 2 || aliveNeighbours > 3 {
+					newWorld[y-startY][x] = 0
+				} else {
+					newWorld[y-startY][x] = 255
+				}
+			} else {
+				if aliveNeighbours == 3 {
+					newWorld[y-startY][x] = 255
+				} else {
+					newWorld[y-startY][x] = 0
+				}
+			}
+		}
+	}
+	return newWorld
+}
+
+// distributor runs the parallel version of the Game of Life (Stage 2).
 func distributor(p Params, c distributorChannels) {
 
-	// TODO: Create a 2D slice to store the world.
+	// 1. Create and read the world from IO.
+	world := make([][]uint8, p.ImageHeight)
+	for y := range world {
+		world[y] = make([]uint8, p.ImageWidth)
+	}
 
-	turn := 0
-	c.events <- StateChange{turn, Executing}
+	filename := strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight)
+	c.ioCommand <- ioInput
+	c.ioFilename <- filename
 
-	// TODO: Execute all turns of the Game of Life.
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			world[y][x] = <-c.ioInput
+		}
+	}
 
-	// TODO: Report the final state using FinalTurnCompleteEvent.
+	// 2. Run for the required number of turns using p.Threads workers.
+	for turn := 0; turn < p.Turns; turn++ {
+		threads := p.Threads
+		rowsPerThread := p.ImageHeight / threads
 
-	// Make sure that the Io has finished any output before exiting.
+		// create one channel per worker to collect their part of the next world
+		results := make([]chan [][]uint8, threads)
+		for i := range results {
+			results[i] = make(chan [][]uint8)
+		}
+
+		// launch each worker goroutine
+		for i := 0; i < threads; i++ {
+			startY := i * rowsPerThread
+			endY := (i + 1) * rowsPerThread
+			if i == threads-1 {
+				endY = p.ImageHeight
+			}
+
+			go func(start, end int, ch chan [][]uint8) {
+				section := nextGrid(p, world, start, end)
+				ch <- section
+			}(startY, endY, results[i])
+		}
+
+		// gather results from workers and rebuild full world
+		newWorld := make([][]uint8, p.ImageHeight)
+		rowIndex := 0
+		for i := 0; i < threads; i++ {
+			part := <-results[i]
+			for _, row := range part {
+				newWorld[rowIndex] = row
+				rowIndex++
+			}
+		}
+
+		world = newWorld
+	}
+
+	// 3. Collect all live cells at the end.
+	var aliveCells []util.Cell
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			if world[y][x] == 255 {
+				aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+
+	// 4. Send final event and finish up.
+	c.events <- FinalTurnComplete{CompletedTurns: p.Turns, Alive: aliveCells}
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
-
-	c.events <- StateChange{turn, Quitting}
-
-	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
+	c.events <- StateChange{CompletedTurns: p.Turns, NewState: Quitting}
 	close(c.events)
 }
