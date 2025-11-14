d8acff38795d58df83e077b9429b756658eb514c
layanalajeel7 <pk23085@bristol.ac.uk>
2025-11-14 02:05:02 +0000

    Initial First Stage

--- gol/distributor.go
+++ gol/distributor.go
@@ -2,11 +2,9 @@ package gol
 
 import (
 	"strconv"
-
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
-// Channels used between Distributor, IO and TestGol.
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -16,52 +14,10 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
-// nextGrid runs for a section of rows (startY to endY) and returns that part of the next world.
-func nextGrid(p Params, world [][]uint8, startY, endY int) [][]uint8 {
-	height := endY - startY
-	newWorld := make([][]uint8, height)
-	for y := 0; y < height; y++ {
-		newWorld[y] = make([]uint8, p.ImageWidth)
-	}
-
-	for y := startY; y < endY; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-
-			aliveNeighbours := 0
-			for i := -1; i <= 1; i++ {
-				for j := -1; j <= 1; j++ {
-					if !(i == 0 && j == 0) {
-						ny := (y + i + p.ImageHeight) % p.ImageHeight
-						nx := (x + j + p.ImageWidth) % p.ImageWidth
-						if world[ny][nx] == 255 {
-							aliveNeighbours++
-						}
-					}
-				}
-			}
-
-			if world[y][x] == 255 {
-				if aliveNeighbours < 2 || aliveNeighbours > 3 {
-					newWorld[y-startY][x] = 0
-				} else {
-					newWorld[y-startY][x] = 255
-				}
-			} else {
-				if aliveNeighbours == 3 {
-					newWorld[y-startY][x] = 255
-				} else {
-					newWorld[y-startY][x] = 0
-				}
-			}
-		}
-	}
-	return newWorld
-}
-
-// distributor runs the parallel version of the Game of Life (Stage 2).
 func distributor(p Params, c distributorChannels) {
 
-	// 1. Create and read the world from IO.
+	turn := 0
+	c.events <- StateChange{turn, Executing}
 	world := make([][]uint8, p.ImageHeight)
 	for y := range world {
 		world[y] = make([]uint8, p.ImageWidth)
@@ -73,63 +29,91 @@ func distributor(p Params, c distributorChannels) {
 
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
-			world[y][x] = <-c.ioInput
+			val := <-c.ioInput
+			world[y][x] = val
+
+			if val == 255 {
+				c.events <- CellFlipped{
+					CompletedTurns: 0,
+					Cell:           util.Cell{X: x, Y: y},
+				}
+			}
 		}
 	}
 
-	// 2. Run for the required number of turns using p.Threads workers.
-	for turn := 0; turn < p.Turns; turn++ {
-		threads := p.Threads
-		rowsPerThread := p.ImageHeight / threads
+	for turn < p.Turns {
 
-		// create one channel per worker to collect their part of the next world
-		results := make([]chan [][]uint8, threads)
-		for i := range results {
-			results[i] = make(chan [][]uint8)
-		}
-
-		// launch each worker goroutine
-		for i := 0; i < threads; i++ {
-			startY := i * rowsPerThread
-			endY := (i + 1) * rowsPerThread
-			if i == threads-1 {
-				endY = p.ImageHeight
-			}
-
-			go func(start, end int, ch chan [][]uint8) {
-				section := nextGrid(p, world, start, end)
-				ch <- section
-			}(startY, endY, results[i])
-		}
-
-		// gather results from workers and rebuild full world
 		newWorld := make([][]uint8, p.ImageHeight)
-		rowIndex := 0
-		for i := 0; i < threads; i++ {
-			part := <-results[i]
-			for _, row := range part {
-				newWorld[rowIndex] = row
-				rowIndex++
-			}
+		for y := range newWorld {
+			newWorld[y] = make([]uint8, p.ImageWidth)
 		}
 
-		world = newWorld
-	}
+		for y := 0; y < p.ImageHeight; y++ {
+			for x := 0; x < p.ImageWidth; x++ {
 
-	// 3. Collect all live cells at the end.
-	var aliveCells []util.Cell
+				aliveNeighbours := 0
+				for dy := -1; dy <= 1; dy++ {
+					for dx := -1; dx <= 1; dx++ {
+						if dy == 0 && dx == 0 {
+							continue
+						}
+
+						ny := (y + dy + p.ImageHeight) % p.ImageHeight
+						nx := (x + dx + p.ImageWidth) % p.ImageWidth
+
+						if world[ny][nx] == 255 {
+							aliveNeighbours++
+						}
+					}
+				}
+				// current state
+				current := world[y][x]
+				next := current
+
+				if current == 255 {
+					if aliveNeighbours < 2 || aliveNeighbours > 3 {
+						next = 0
+					}
+				} else {
+					if aliveNeighbours == 3 {
+						next = 255
+					}
+				}
+
+				newWorld[y][x] = next
+				if next != current {
+					c.events <- CellFlipped{
+						CompletedTurns: turn + 1,
+						Cell:           util.Cell{X: x, Y: y},
+					}
+				}
+			}
+		}
+		// move to next world
+		world = newWorld
+		turn++
+
+		// turn complete (SDL draws a frame)
+		c.events <- TurnComplete{CompletedTurns: turn}
+	}
+	var alive []util.Cell
+
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
 			if world[y][x] == 255 {
-				aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
+				alive = append(alive, util.Cell{X: x, Y: y})
 			}
 		}
 	}
+	c.events <- FinalTurnComplete{
+		CompletedTurns: turn,
+		Alive:          alive,
+	}
 
-	// 4. Send final event and finish up.
-	c.events <- FinalTurnComplete{CompletedTurns: p.Turns, Alive: aliveCells}
+	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
-	c.events <- StateChange{CompletedTurns: p.Turns, NewState: Quitting}
+
+	c.events <- StateChange{CompletedTurns: turn, NewState: Quitting}
 	close(c.events)
 }
